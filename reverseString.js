f26a298716a49b3cb1ef975cf19979a11d117a87
Anshul Jain <anshul.jain@gwynniebee.com>
2020-05-27 02:56:14 +0530

    Reverse a string

---
+++ reverseString.js
@@ -0,0 +1,45 @@
+// Create a function to reverse a string
+
+/*
+    reverse("Hello, World!"); // !dlroW ,olleH
+*/
+
+function reverse (str) {
+    var arr = str.split(""); // split in characters
+    return arr.reverse().join("");
+}
+
+// console.log(reverse("Anshul, hi!"));
+
+function reverse2 (str) {
+    var arr = str.split("");
+    for(let i = 0, j = arr.length - 1; i < j; i++, j--) {
+        var temp = arr[j];
+        arr[j] = arr[i];
+        arr[i] = temp;
+    }
+    return arr.join("");
+}
+
+// console.log(reverse2("Anshul, hi!"));
+
+function reverse3 (str) {
+    var newString = "";
+    for(let i = str.length - 1; i >= 0; i--) {
+        newString += str.charAt(i);
+    }
+    return newString;
+}
+
+// console.log(reverse3("Anshul, hi!"));
+
+// function reverse4 (str) {
+//     for(let i = 0, j = str.length - 1; i < j; i++, j--) {
+//         var temp = str.charAt(j);
+//         str.charAt(j) = str.charAt(i); // DOES NOT WORK
+//         str.charAt(i) = temp;
+//     }
+//     return str;
+// }
+
+// console.log(reverse4("Anshul, hi!"));
