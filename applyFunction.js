d2ff28e82fa75dde9a7671d375885c0c86577c44
Anshul Jain <anshul.jain@gwynniebee.com>
2020-05-28 02:16:36 +0530

    apply function

---
+++ applyFunction.js
@@ -0,0 +1,18 @@
+// Implement Function.prototype.apply()
+
+/*
+    const foo = function (msg1, msg2) {
+        console.log(msg1 + ' ' + msg2 + ' ' + this.bar);
+    }
+
+    foo.apply({bar: 'Anshul'}, ['hello', 'world']); // hello world Anshul
+*/
+
+Function.prototype.apply = function(newContext) {
+    // point 'this' to newContext's scope and call the function
+    newContext.fn = this;
+
+    var args = arguments[1];
+
+    newContext.fn(...args);
+};
