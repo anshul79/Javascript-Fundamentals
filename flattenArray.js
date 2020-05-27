3adb28b6c48921cc1a5f8ba05bee0b3a2c77a8fd
Anshul Jain <anshul.jain@gwynniebee.com>
2020-05-27 15:38:23 +0530

    Flatten Array

---
+++ flattenArray.js
@@ -0,0 +1,34 @@
+// Withour using .flat(), create a function to flatten an array
+
+// const exampleArray = [1, 2, [3, 4, [5, 6, 7], 8], 9, 10]
+// flatten(exampleArray); // [1, 2, 3, 4, 5, 6, 7, 8, 8, 10];
+
+function flatten(array) {
+    let newArray = [];
+    for(let i = 0; i < array.length; i++) {
+        if(typeof array[i] == "object") {
+            newArray = newArray.concat(flatten(array[i]));
+        } else {
+            newArray.push(array[i]);
+        }
+    }
+    return newArray;
+}
+
+var exampleArray = [1, 2, [3, 4, [5, 6, 7], 8], 9, 10];
+// console.log(flatten(exampleArray));
+
+function flatten2(array) {
+    let newArray = [];
+    newArray = array.reduce((acc, item) => {
+        if(Array.isArray(item)) {
+            acc = acc.concat(flatten2(item));
+        } else {
+            acc.push(item);
+        }
+        return acc;
+    }, []);
+    return newArray;
+}
+
+// console.log(flatten2(exampleArray));
