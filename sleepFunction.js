ec13129befb09c593c9551d98c16dfe4959208b4
Anshul Jain <anshul.jain@gwynniebee.com>
2020-05-28 00:04:06 +0530

    Sleep function

---
+++ sleepFunction.js
@@ -0,0 +1,19 @@
+// Create a sleep function that takes one parameter (time) and 
+// will wait "time" ms
+
+/*
+    async function run() {
+        await sleep(500);
+        console.log("Hello");
+        await sleep(500);
+        console.log(" world");
+    }
+*/
+
+function sleep(time) {
+    return new Promise((resolve) => {
+        setTimeout(() => {
+            resolve();
+        }, time);
+    });
+}
