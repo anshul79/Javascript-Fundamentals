aca9e8ee50f4d6b3b301e2473be7912ccdca5675
Anshul Jain <anshul.jain@gwynniebee.com>
2020-05-30 14:23:42 +0530

    Another implementation of bind

--- bindFunction.js
+++ bindFunction.js
@@ -29,6 +29,28 @@ Function.prototype.bind = function(newContext) {
             args2.push(arguments[i]);
         }
 
-        fn.apply(newContext, args.concat(args2));
+        return fn.apply(newContext, args.concat(args2));
+    };
+};
+
+
+// Second implementation
+var slice = Array.prototype.slice;
+
+Function.prototype.bind2 = function(newContext) {
+    // point 'this' to newContext's scope
+    if(typeof this != 'function') {
+        throw new Error(this + ' cannot be bound as it is not callable');
+    }
+
+    const fn = this;
+
+    let args = slice.call(arguments, 1);
+    // slice() has been used like this because it is by default available as Array.prototype.slice()
+    // but here we are using it on an object 'arguments' and returning the new array
+
+    return function () {
+        let args2 = slice.call(arguments);
+        return fn.apply(newContext, args.concat(args2));
     };
 };
